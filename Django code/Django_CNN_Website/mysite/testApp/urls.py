e403dda707f0929d21819b2e9aeb5ef19a69d3fa
Rohit Agarwal <agarwal.ro@husky.neu.edu>
2017-04-24 05:18:26 -0400

    Updated Code

--- Django code/Django_CNN_Website/mysite/testApp/urls.py
+++ Django code/Django_CNN_Website/mysite/testApp/urls.py
@@ -1,10 +1,20 @@
 from django.conf.urls import url
-
+from django.conf import settings
 from testApp import views
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
     url(r'^about/$', views.AboutPageView.as_view()),
     url(r'^getData/$', views.getDataView.as_view()),
-    url(r'^showData/$', views.index)
+    url(r'^showData/$', views.index),
+    url(r'^simulate/$', views.simulateView.as_view()),
+    url(r'^simulateData/$', views.index1),
+    url(r'^staticData/$', views.index2),
+    url(r'^predictData/$', views.index3),
 ]
+
+
+
+# stuff
+
+(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
--- Django code/Django_CNN_Website/mysite/testApp/views.py
+++ Django code/Django_CNN_Website/mysite/testApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.conf import settings
+import shutil
 from django.views.generic import TemplateView
+from django.views.generic import CreateView
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
@@ -16,6 +19,7 @@ from keras import optimizers
 from keras import applications
 from keras.models import Model
 import math
+from fractions import gcd
 from keras.utils import np_utils
 import shutil
 import time
@@ -23,7 +27,13 @@ from shutil import copyfile
 from time import sleep
 import urllib.request as urllib2
 from PIL import Image
+from django.core.files.storage import FileSystemStorage
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from django.core.cache import cache
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.template import RequestContext
+import tensorflow as tf
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -34,18 +44,10 @@ class HomePageView(TemplateView):
 class AboutPageView(TemplateView):
     template_name = "about.html"
     
+    
 # new Class
 class getDataView(TemplateView):
     template_name = "getData.html"
-
-def gcd(a, b):
-    if(a<b):
-        a,b=b,a
-    while(b!=0):
-        r=b
-        b=a%r
-        a=r
-    return a
     
 #disabling csrf (cross site request forgery)
 numImage = [1,11,21,31,41,51,61,71,81,91]
@@ -57,19 +59,21 @@ trainPath = cwd+"/Data/Train/"
 valiPath = cwd+"/Data/Validation/"
 trainSize = sum([len(files) for r, d, files in os.walk(trainPath)])
 valiSize = sum([len(files) for r, d, files in os.walk(valiPath)])
-batch_size = gcd(trainSize, valiSize)
-print(batch_size)
-epochs = 2
+batch_size = math.gcd(trainSize, valiSize)
+epochs = 1
+#train Model
+#preTrainModel = 0
+#nameClass = None
 #urlImage = []
 def getImage(name, urlImage):
     
     for y in range(len(numImage)):
         service = build("customsearch", "v1",
-               developerKey="AIzaSyBK07xoWr9EvTD888oA7EtZGnyslPdiO84")
+               developerKey="AIzaSyBaSIN22OfPg9Fdf5SbCmPlwG9nK-jpvl4")
 
         res = service.cse().list(
             q=name,
-            cx='005673961504823902730:9y0jxagt1qc',
+            cx='000946463977679166157:8srxwerixf4',
             searchType = "image",
             start = numImage[y],
             num=10,
@@ -154,6 +158,7 @@ def trainBottleNeck(train_data_dir, valiPath, img_width, img_height, model_vgg, 
     
 def validateBottleNeck(train_data_dir, validation_data_dir, img_width, img_height, model_vgg, validation_samples, train_data, train_labels, train_samples, numClass):
     print("Validate Bottleneck")
+    global preTrainModel
     validation_generator_bottleneck = datagen.flow_from_directory(
         validation_data_dir,
         target_size=(img_width, img_height),
@@ -233,13 +238,21 @@ def validateBottleNeck(train_data_dir, validation_data_dir, img_width, img_heigh
     epochs=epochs,
     validation_data=validation_generator,
     validation_steps=validation_samples // batch_size)
-    model.save_weights(cwd+'/Models/Test_finetuning_30epochs_vgg.h5')
-    fname = glob.glob(cwd+"/Data/Train/cat/*.jpg")
-    for i in range(len(fname)):    
-        img = load_img(fname[i],False, (img_width, img_height))
-        x = img_to_array(img)
-        prediction = model.predict(x.reshape((1,img_width, img_height,3)),batch_size=32, verbose=0)
-        print(np.round(prediction))
+    model.save_weights(cwd+'/Models/PreTrained_finetuning_30epochs_vgg.h5')
+    
+    # Don't fuck with this below line.
+    # else will fuck you.
+    
+    global graph
+    graph = tf.get_default_graph()
+            
+    preTrainModel = model
+    print(preTrainModel)
+    print("*"*50)
+    print(preTrainModel.input_shape)
+    print("*"*50)
+    print(preTrainModel.output_shape)
+    print("Model Trained")
     
     
     
@@ -247,7 +260,11 @@ def beginProcessing(trainPath, valiPath):
     print("Start Processing")
     train_labels, nameOfClasses = fcount(trainPath)
     validation_labels, nameOfClasses = fcount(valiPath)
+    nameOfClasses.sort()
     numClass = train_labels
+    
+    global nameClass
+    nameClass = nameOfClasses
     ##preprocessing
     # used to rescale the pixel values from [0, 255] to [0, 1] interval
     
@@ -299,6 +316,7 @@ def index(request):
     if request.method == 'POST':
         #getting values from post
         name = request.POST.get('name')
+        train = 'Your Model is Successfully Trained'
         urlImage = []
         #getImage(name, urlImage)
         if len(urlImage) == 0:
@@ -309,9 +327,9 @@ def index(request):
         #adding the values in a context variable 
         context = {
             'name': name,
+            'train' : train
         }
         
-            
         template = loader.get_template('showData.html')
         
         #returing the template 
@@ -321,3 +339,160 @@ def index(request):
         #returing the form template 
         template = loader.get_template('index.html')
         return HttpResponse(template.render())
+    
+    
+# new Class
+class simulateView(TemplateView):
+    template_name = "simulate.html"
+    
+def predictResult(preTrainModel,  filePath, nameClass):
+    print("*"*50)
+    print(preTrainModel)
+    print("*"*50)
+    print(nameClass)
+    print("*"*50)
+    print(filePath)
+    print("*"*50)
+    print(preTrainModel.input_shape)
+    img = load_img(filePath, False, (img_width, img_height))
+    x = img_to_array(img)
+    print("*"*50)
+    print(x.shape)
+    with graph.as_default():
+        predictions = preTrainModel.predict(x.reshape((1,img_width, img_height,3)),batch_size = batch_size, verbose=0)
+    print("*"*50)
+    print(preTrainModel.output_shape)
+    result = dict()
+    i = 0
+    for x in predictions.tolist()[0]:
+        result[nameClass[i]] = round(x,4)
+        i+=1
+    global finalResult
+    finalResult = result
+    
+@csrf_exempt
+def index1(request):
+    if request.method == 'POST' and request.FILES['imageName']:
+        #getting values from post
+        myfile = request.FILES['imageName']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        filePath = cwd + "/testApp/templates/static/"+ filename
+        print(filePath)
+        if preTrainModel != None:
+            predictResult(preTrainModel, filePath, nameClass)
+        else:
+            print("Model not Pre Trained")
+        #dst = cwd + "/testApp/templates/static/" + filename
+        #copyfile(filePath, dst)
+        template = loader.get_template('simulate.html')
+        context = {
+            'file': myfile,
+            'fResult' : finalResult
+        }
+        #returing the template 
+        return HttpResponse(template.render(context, request))
+    else:
+        #if post request is not true 
+        #returing the form template 
+        template = loader.get_template('index.html')
+        return HttpResponse(template.render())
+
+    
+def trainStaticModel():
+    print("Started Training Model")
+    newmodel = applications.VGG16(weights='imagenet', include_top=False, input_shape=(150, 150, 3))
+    top_model = Sequential()
+    top_model.add(Flatten(input_shape=newmodel.output_shape[1:]))
+    top_model.add(Dense(256, activation='relu'))
+    top_model.add(Dropout(0.5))
+    top_model.add(Dense(4, activation='softmax'))
+    top_model.load_weights('staticModel/Test_bottleneck_30_epochs.h5')
+    model = Model(inputs = newmodel.input, outputs = top_model(newmodel.output))
+    for layer in model.layers[:15]:
+        layer.trainable = False
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+                  metrics=['accuracy'])
+    model.load_weights('staticModel/FinalStaticModel_30epochs_vgg.h5')
+    global graph1
+    graph1 = tf.get_default_graph()
+    
+    global staticWeight
+    print("Static Model Trained")
+    staticWeight = model
+    print(staticWeight)
+    
+    
+    
+def staticPrediction(staticWeight, filePath):
+    nameClass = ['EarthQuake', 'Hurricane', 'Tornado', 'Volcanic Erruption']
+    img = load_img(filePath, False, (150, 150))
+    x = img_to_array(img)
+    with graph1.as_default():
+        predictions2 = staticWeight.predict(x.reshape((1,150, 150,3)),batch_size = 40, verbose=0)
+    result = dict()
+    i = 0
+    for x in predictions2.tolist()[0]:
+        result[nameClass[i]] = round(x,4)
+        i+=1
+    #global staticResult
+    print(result)
+    return result   
+
+    
+####### Load the static model #########    
+@csrf_exempt
+def index2(request):
+    if request.method == 'POST':
+        #getting values from post
+        val = request.POST.get('predict')
+        trainStaticModel()
+        #adding the values in a context variable 
+        context = {
+            'name': staticWeight,
+        }
+        
+        template = loader.get_template('simulateData.html')
+        
+        #returing the template 
+        return HttpResponse(template.render(context, request))
+    else:
+        #if post request is not true 
+        #returing the form template 
+        template = loader.get_template('index.html')
+        return HttpResponse(template.render()) 
+
+###### Predict static model ############
+@csrf_exempt
+def index3(request):
+    print("*"*50)
+    print(staticWeight)
+    if request.method == 'POST' and request.FILES['staticImage']:
+        #getting values from post
+        myfile = request.FILES['staticImage']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        filePath = cwd + "/testApp/templates/static/"+ filename
+        
+        if staticWeight != None:
+            result = staticPrediction(staticWeight, filePath)
+        else:
+            print("Static Model Not Trained")
+        
+        template = loader.get_template('simulateData.html')
+        context = {
+            'file': myfile,
+            'result': result
+        }
+        #returing the template 
+        return HttpResponse(template.render(context, request))
+    else:
+        #if post request is not true 
+        #returing the form template 
+        template = loader.get_template('index.html')
+        return HttpResponse(template.render())
+    
+    
